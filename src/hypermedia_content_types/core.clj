40164a52956880933b1cb0c324706eab6b218729
Logan Campbell <colin@logaan.net>
2013-09-14 15:04:55 +1000

    secret resource

--- src/hypermedia_content_types/core.clj
+++ src/hypermedia_content_types/core.clj
@@ -1,17 +1,27 @@
 (ns hypermedia-content-types.core
   (:use [org.httpkit.server :only [run-server]])
   (:require [ring.middleware.reload :as reload]
-            [liberator.core :refer  [defresource]]
+            [liberator.core :refer  [resource defresource]]
             [compojure.route :as route] 
             [compojure.handler :refer [site]]
-            [compojure.core :refer [defroutes GET POST]]))
+            [compojure.core :refer [defroutes ANY GET POST]]))
 
 (defresource index
   :available-media-types ["text/html"]
-  :handle-ok "<html>Hello, Internet.</html>")
+  :handle-ok (fn [ctx]
+               (format "<html>It's %d milliseconds since the beginning of the
+                       epoch." (System/currentTimeMillis))))
+
+(defresource secret
+  :available-media-types ["text/html"]
+  :exists? (fn [ctx]
+             (= "tiger" (get-in ctx [:request :params :word])))
+  :handle-ok "You found the secret word!"
+  :handle-not-found "Uh, that's the wrong word. Guess again!" )
 
 (defroutes all-routes
-  (GET "/" [] index)
+  (ANY "/" [] index)
+  (ANY "/secret" [] secret)
   (route/not-found "<p>Page not found.</p>"))
 
 (def reloading-handler
